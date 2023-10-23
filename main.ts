fd2da220f2c7cce6cd556ed83e476bcfc7b40262
t0m0t0w <57962470+t0m0t0w@users.noreply.github.com>
2023-10-23 13:15:06 +0200

    vi er best gruppe

--- main.ts
+++ main.ts
@@ -1,32 +1,45 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        wooweeewooweee = 0
+    } else if (receivedNumber == 2) {
+        bitbot.stop(BBStopMode.Coast)
+    } else if (receivedNumber == 3) {
+        stor_fart = 30
+        liten_fart = 15
+    } else if (receivedNumber == 4) {
+        stor_fart = 10
+        liten_fart = 5
+    } else if (receivedNumber == 5) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
 input.onButtonPressed(Button.A, function () {
     wooweeewooweee = 0
 })
 input.onButtonPressed(Button.B, function () {
     wooweeewooweee = 1
 })
+let liten_fart = 0
+let stor_fart = 0
 let wooweeewooweee = 0
 bitbot.select_model(BBModel.XL)
 let weeewooweeewoo = 40
 basic.forever(function () {
     if (wooweeewooweee == 0) {
-        if (bitbot.readLight(BBLightSensor.Left) + bitbot.readLight(BBLightSensor.Right) > weeewooweeewoo) {
-            if (bitbot.readLight(BBLightSensor.Left) > bitbot.readLight(BBLightSensor.Right)) {
-                bitbot.rotate(BBRobotDirection.Left, 60)
-            } else if (bitbot.readLight(BBLightSensor.Left) < bitbot.readLight(BBLightSensor.Right)) {
-                bitbot.rotate(BBRobotDirection.Right, 60)
-            }
-        } else {
-            bitbot.go(BBDirection.Forward, 60)
+        if (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 0) {
+            bitbot.move(BBMotor.Left, BBDirection.Forward, liten_fart)
+            bitbot.move(BBMotor.Right, BBDirection.Forward, stor_fart)
+        } else if (bitbot.readLine(BBLineSensor.Right) == 1 && bitbot.readLine(BBLineSensor.Left) == 0) {
+            bitbot.move(BBMotor.Left, BBDirection.Forward, stor_fart)
+            bitbot.move(BBMotor.Right, BBDirection.Forward, liten_fart)
+        } else if (bitbot.readLine(BBLineSensor.Right) == 1 && bitbot.readLine(BBLineSensor.Left) == 1) {
+        	
         }
     } else if (wooweeewooweee == 1) {
-        if (bitbot.readLine(BBLineSensor.Left) == 1 && bitbot.readLine(BBLineSensor.Right) == 0) {
-            bitbot.move(BBMotor.Left, BBDirection.Forward, 10)
-            bitbot.move(BBMotor.Right, BBDirection.Forward, 20)
-        } else if (bitbot.readLine(BBLineSensor.Right) == 1 && bitbot.readLine(BBLineSensor.Left) == 0) {
-            bitbot.move(BBMotor.Left, BBDirection.Forward, 20)
-            bitbot.move(BBMotor.Right, BBDirection.Forward, 10)
-        } else if (bitbot.readLine(BBLineSensor.Right) == 1 && bitbot.readLine(BBLineSensor.Left) == 1) {
-            bitbot.stop(BBStopMode.Coast)
-        }
+    	
     }
 })
